107a4cccc3c5892b8fe2248fa245ae1360c80233
John Doe <kurbanof.atabek@gmail.com>
2021-09-24 02:53:45 +0600

    created commit and action for getting Pizzas from API.

--- src/components/PizzaList.vue
+++ src/components/PizzaList.vue
@@ -1,25 +1,26 @@
 <template>
 	<h2 class="content__title">Все пиццы</h2>
 	<div class="content__items">
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
-		<PizzaItem />
+		<PizzaItem v-for="pizza in pizzas" :key="pizza.id" :pizza="pizza" />
 	</div>
 </template>
 
 <script>
 import PizzaItem from "./SinglePizza.vue";
+import { useStore } from "vuex";
+import { computed } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
 export default {
 	components: { PizzaItem },
+	setup() {
+		const store = useStore();
+
+		onMounted(() => store.dispatch("getPizzasAPI"));
+
+		const pizzas = computed(() => store.state.pizzas);
+
+		return { pizzas };
+	},
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		pizzas: [],
+	},
+	mutations: {
+		GET_PIZZAS(state, { pizzas }) {
+			state.pizzas = [...state.pizzas, ...pizzas]
+		},
+	},
+	actions: {
+		async getPizzasAPI({ commit }) {
+			const response = await fetch('http://localhost:3000/db.json')
+			const pizzas = await response.json()
+			commit('GET_PIZZAS', pizzas)
+		},
+	},
+	modules: {},
 })
